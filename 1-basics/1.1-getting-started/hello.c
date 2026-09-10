a8047c368cf81b631140ec99f5f69ea70cd1266a
Egha Kuncoro <eghakuncorozxzxz@gmail.com>
2026-09-10 11:42:03 +0700

    Complete task 1-basics/1.1-getting-started/hello.c

---
+++ 1-basics/1.1-getting-started/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // tells the compiler to include information about the standard input/output library
+
+int main()
+{
+    /*
+    define a function called main that received no argument values 
+    statements of main are enclosed in braces main calls library 
+    function printf to print this sequence of characters
+    \n represents the newline character
+    */
+    printf("Hello World!\n");
+    
+    return 0; // return 0 indicates the program has completed without errors
+
+}
